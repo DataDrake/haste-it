70834669a0dc6bd2e7890f9e06976295dfe44105
Bryan T. Meyers <bmeyers@datadrake.com>
2016-12-29 03:47:42 -0500

    go fmt

--- haste.go
+++ haste.go
@@ -17,54 +17,54 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
-    "os"
+	"encoding/json"
+	"net/http"
+	"os"
 )
 
 func haste(f *os.File) {
-    c := &http.Client{}
-    r, err := c.Post("https://hastebin.com/documents", "application/json", f)
-    if err != nil {
-        println("Failed to send, reason: " + err.Error())
-        os.Exit(1)
-    }
-    d := json.NewDecoder(r.Body)
-    var v struct {
-        Key string
-    }
-    err = d.Decode(&v)
-    if err != nil {
-        println("Failed to parse response")
-        os.Exit(1)
-    }
-    println("https://hastebin.com/" + v.Key)
+	c := &http.Client{}
+	r, err := c.Post("https://hastebin.com/documents", "application/json", f)
+	if err != nil {
+		println("Failed to send, reason: " + err.Error())
+		os.Exit(1)
+	}
+	d := json.NewDecoder(r.Body)
+	var v struct {
+		Key string
+	}
+	err = d.Decode(&v)
+	if err != nil {
+		println("Failed to parse response")
+		os.Exit(1)
+	}
+	println("https://hastebin.com/" + v.Key)
 }
 
 func hasteFile(fp string) {
-    f,err := os.Open(fp)
-    if err != nil {
-        println("Failed to open file: '" + fp + "', reason: " + err.Error())
-        os.Exit(1)
-    }
-    defer f.Close()
-    haste(f)
+	f, err := os.Open(fp)
+	if err != nil {
+		println("Failed to open file: '" + fp + "', reason: " + err.Error())
+		os.Exit(1)
+	}
+	defer f.Close()
+	haste(f)
 }
 
 func usage() {
-    print("haste-it --- Simple little client for hastebin\n\n")
-    print("Usage: haste [file]\n\n")
-    print("    Read from [file] or stdin\n\n")
+	print("haste-it --- Simple little client for hastebin\n\n")
+	print("Usage: haste [file]\n\n")
+	print("    Read from [file] or stdin\n\n")
 }
 
 func main() {
-    switch len(os.Args) {
-    case 1:
-        haste(os.Stdin)
-    case 2:
-        hasteFile(os.Args[1])
-    default:
-        usage()
-        os.Exit(1)
-    }
+	switch len(os.Args) {
+	case 1:
+		haste(os.Stdin)
+	case 2:
+		hasteFile(os.Args[1])
+	default:
+		usage()
+		os.Exit(1)
+	}
 }
